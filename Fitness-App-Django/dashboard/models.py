49374cf81f2feda69272bf9d46245655861616bb
kumikorandle <kcrandle98@hotmail.com>
2020-04-25 17:08:11 -0400

    Updated exercise model and home page
    Added exercises and muscles worked to exercise model, used multiselectfield for muscles worked. Updated home page to show date joined and workouts count (showing up as blank instead of 0 right now?)

--- Fitness-App-Django/dashboard/models.py
+++ Fitness-App-Django/dashboard/models.py
@@ -1,19 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import User
+from multiselectfield import MultiSelectField
 
 class Exercise(models.Model):
 	TYPES = (
 		('bicep-curls', 'Bicep Curls'),
 		('squats', 'Squats'),
 		('jumping-jacks', 'Jumping Jacks'),
+		('bench-press', 'Bench Press'),
+		('bulgarian-split-squat', 'Bulgarian Split Squat'),
+		('chest-fly', 'Chest Fly'),
+		('crunches', 'Crunches'),
+		('curtsy-lunge', 'Curtsy Lunge'),
+		('good-morning', 'Good Morning'),
+		('hip-abduction', 'Hip Abduction'),
+		('hip-thrusts', 'Hip Thrusts'),
+		('leg-extension', 'Leg Extension'),
+		('leg-press', 'Leg Press'),
+		('lying-leg-curl', 'Lying Leg Curl'),
+		('rdl', 'RDL'),
+		('russian-twists', 'Russian Twists'),
+		('sumo-deadlift', 'Sumo Deadlift'),
+		('tricep-extension', 'Tricep Extension'),
 	)
 	MUSCLES = (
+		('abs', 'Abs'),
+		('back', 'Back'),
 		('biceps', 'Biceps'),
-		('legs', 'Legs'),
-		('cardio', 'Cardio')
+		('calves', 'Calves'),
+		('chest', 'Chest'),
+		('forearms', 'Forearms'),
+		('glutes', 'Glutes'),
+		('hamstrings', 'Hamstrings'),
+		('quadriceps', 'Quadriceps'),
+		('shoulders', 'Shoulders'),
+		('traps', 'Traps'),
+		('triceps', 'Triceps'),
+		('upper-back', 'Upper Back'),
+		('lower-back', 'Lower Back'),
+		
 	)
 	name = models.CharField(max_length=120, choices = TYPES, default = 'bicep-curls')
-	muscles_worked = models.CharField(max_length=120, choices = MUSCLES, default = 'biceps')
+	muscles_worked = MultiSelectField(max_length=120, choices = MUSCLES, default = 'biceps')
 	num_reps = models.IntegerField()
 	num_sets = models.IntegerField()
 	video_link = models.URLField(max_length=500)
